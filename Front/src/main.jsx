3c7805290fec2cc4db456d268c6f129d9336c6e6
Tupap1 <andresdavidcantillo@gmail.com>
2024-10-08 07:02:05 -0500

    asd

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,25 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Entrar from './components/login.jsx'
+
 import './components/styles/index.css'
 import App from './App.jsx' 
 import Formulario from './RegistrarDatosdeBiometria.jsx'
 import Biometria from './ResgisterBiometrias.jsx'
-import Menu from './SideBarMenu.tsx'
-import FcAdvertising from 'react-icons/fc'
+import Boton from './components/ui/Boton.tsx'
+import Form from './components/ui/Form.tsx'
+import SideBar from './components/ui/SideBar.tsx'
 
 import React from 'react'
-
-export default function hola() {
-  return (
-    <div>hola</div>
-  )
-}
-
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Menu />
-  </StrictMode>,
+<SideBar/>
+  </StrictMode>
 )
